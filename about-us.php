d7cb3c9d2f4d9172cf079c2f7b90335010074560
Momi786 <ramaljan786@gmail.com>
2023-06-16 00:13:15 +0500

    new page updated

---
+++ about-us.php
@@ -0,0 +1,221 @@
+<?php include('include/header.php') ?>
+
+<!-- About us section code starts here  -->
+
+<section class="about__us__section">
+    <div class="container">
+        <div class="row">
+            <div class="about__us__code__heading">
+                <h1>The Box Masters:<br> 
+                Pioneering Custom Packaging Solutions</h1>
+            </div>
+            <div class="about__us__images__area row">
+                <div class="col-xs-3 col-sm-3 col-md-3 col-lg-3">
+                    <img src="asset/images/about__us/about__us__image1.png" class="image__style1 img-fluid" alt="">
+                </div>
+                <div class="col-xs-3 col-sm-3 col-md-3 col-lg-3">
+                    <img src="asset/images/about__us/about__us__image2.png" class="image__style2 img-fluid" alt="">
+                </div>
+                <div class="col-xs-3 col-sm-3 col-md-3 col-lg-3">
+                    <img src="asset/images/about__us/about__us__image3.png" class="image__style3 img-fluid" alt="">
+                </div>
+                <div class="col-xs-3 col-sm-3 col-md-3 col-lg-3">
+                    <img src="asset/images/about__us/about__us__image4.png" class="image__style4 img-fluid" alt="">
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+<!-- About us section code ends here  -->
+
+<!-- about__us__design__section code starts here  -->
+<section class="about__us__design__section">
+    <div class="container">
+        <div class="row">
+            <div class="col-xs-12 col-sm-12 col-md-7 col-lg-7 p-3">
+                <div class="about__us__design__area p-5">
+                    <h3>Unveiling Our Story: Crafting Exquisite Custom Boxes</h3>
+                    <p>we are truly passionate about creating and printing beautiful luxury packaging so whatever your design idea, complex or simple we want to help you and with so many different paper boards and print finishes why not try something new, we can help and advise you.<br>
+                    it’s our aim to make you standout, to make you be remembered; all that is possible with our bespoke luxury printed packaging. each order is given due care and attention from our craftsmen to ensure superior quality, expert finish and maximum impact.</p>
+                </div>
+            </div>
+            <div class="col-xs-12 col-sm-12 col-md-5 col-lg-5 p-3">
+                <div class="about__us__design__image text-center">
+                    <img src="asset/images/about__us/about__design__image2.png" class="img-fluid" alt="">
+                </div>
+            </div>
+            <div class="col-xs-12 col-sm-12 col-md-5 col-lg-5 p-3">
+                <div class="about__us__design__image text-center">
+                    <img src="asset/images/about__us/about__design__image1.png" class="img-fluid" alt="">
+                </div>
+            </div>
+            <div class="col-xs-12 col-sm-12 col-md-7 col-lg-7 p-3">
+                <div class="about__us__design__area p-5">
+                    <h3>Unveiling Our Story: Crafting Exquisite Custom Boxes</h3>
+                    <p>we are truly passionate about creating and printing beautiful luxury packaging so whatever your design idea, complex or simple we want to help you and with so many different paper boards and print finishes why not try something new, we can help and advise you.<br>
+                    it’s our aim to make you standout, to make you be remembered; all that is possible with our bespoke luxury printed packaging. each order is given due care and attention from our craftsmen to ensure superior quality, expert finish and maximum impact.</p>
+                </div>
+            </div>
+            <div class="col-xs-12 col-sm-12 col-md-7 col-lg-7 p-3">
+                <div class="about__us__design__area p-5">
+                    <h3>Unveiling Our Story: Crafting Exquisite Custom Boxes</h3>
+                    <p>we are truly passionate about creating and printing beautiful luxury packaging so whatever your design idea, complex or simple we want to help you and with so many different paper boards and print finishes why not try something new, we can help and advise you.<br>
+                    it’s our aim to make you standout, to make you be remembered; all that is possible with our bespoke luxury printed packaging. each order is given due care and attention from our craftsmen to ensure superior quality, expert finish and maximum impact.</p>
+                </div>
+            </div>
+            <div class="col-xs-12 col-sm-12 col-md-5 col-lg-5 p-3">
+                <div class="about__us__design__image text-center">
+                    <img src="asset/images/about__us/about__design__image2.png" class="img-fluid" alt="">
+                </div>
+            </div>
+            <div class="col-xs-12 col-sm-12 col-md-5 col-lg-5 p-3">
+                <div class="about__us__design__image text-center">
+                    <img src="asset/images/about__us/about__design__image1.png" class="img-fluid" alt="">
+                </div>
+            </div>
+            <div class="col-xs-12 col-sm-12 col-md-7 col-lg-7 p-3">
+                <div class="about__us__design__area p-5">
+                    <h3>Unveiling Our Story: Crafting Exquisite Custom Boxes</h3>
+                    <p>we are truly passionate about creating and printing beautiful luxury packaging so whatever your design idea, complex or simple we want to help you and with so many different paper boards and print finishes why not try something new, we can help and advise you.<br>
+                    it’s our aim to make you standout, to make you be remembered; all that is possible with our bespoke luxury printed packaging. each order is given due care and attention from our craftsmen to ensure superior quality, expert finish and maximum impact.</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<!-- about__us__design__section code ends here  -->
+
+<!-- our Review section Code starts here-->
+
+<section class="our__review">
+    <div class="container">
+        <div class="row">
+            <h2>See Our Reviews</h2>
+            <div class="row">
+                <div class="col-6">
+                    <p>Unlock the potential of your products with our unrivaled packaging solutions tailored to your industry and specifi.</p>
+                </div>
+                <div class="col-6 text-end">
+                    <a href="#">
+                        <button class="btn__layout1"><span>View All Reviews</span></button>
+                    </a>
+                </div>
+            </div>
+        </div>
+        <div class="owl-carousel owl-theme" id="review__slider">
+            <div class="item">
+                <div class="our__review__items">
+                    <div class="d-flex our__review__star">
+                        <img src="asset/images/our__review/star.png" class="img-fluid" alt="">
+                        <img src="asset/images/our__review/star.png" class="img-fluid" alt="">
+                        <img src="asset/images/our__review/star.png" class="img-fluid" alt="">
+                        <img src="asset/images/our__review/star.png" class="img-fluid" alt="">
+                        <img src="asset/images/our__review/star.png" class="img-fluid" alt="">
+                    </div>
+                    <p>“Our members are so impressed. It's intuitive. It's clean. It's distraction free. If you're building a community.</p>
+                    <div class="row our__review__people">
+                        <div class="d-flex">
+                            <div class="our__review__image">
+                                <img src="asset/images/our__review/people1.png" class="img-fluid" alt="">
+                            </div>
+                            <div class="our__review__text">
+                                <h4>Sabo Masties</h4>
+                                <span>Founder @ Rolex</span>
+                            </div>
+                        </div>
+                      
+                    </div>
+                </div>
+            </div>
+            <div class="item">
+                <div class="our__review__items our__review__bg">
+                    <div class="d-flex our__review__star">
+                        <img src="asset/images/our__review/star.png" class="img-fluid" alt="">
+                        <img src="asset/images/our__review/star.png" class="img-fluid" alt="">
+                        <img src="asset/images/our__review/star.png" class="img-fluid" alt="">
+                        <img src="asset/images/our__review/star.png" class="img-fluid" alt="">
+                        <img src="asset/images/our__review/star.png" class="img-fluid" alt="">
+                    </div>
+                    <p>“Our members are so impressed. It's intuitive. It's clean. It's distraction free. If you're building a community.</p>
+                    <div class="row our__review__people">
+                        <div class="d-flex">
+                            <div class="our__review__image">
+                                <img src="asset/images/our__review/people1.png" class="img-fluid" alt="">
+                            </div>
+                            <div class="our__review__text">
+                                <h4>Sabo Masties</h4>
+                                <span>Founder @ Rolex</span>
+                            </div>
+                        </div>
+                      
+                    </div>
+                </div>
+            </div>
+            <div class="item">
+                <div class="our__review__items">
+                    <div class="d-flex our__review__star">
+                        <img src="asset/images/our__review/star.png" class="img-fluid" alt="">
+                        <img src="asset/images/our__review/star.png" class="img-fluid" alt="">
+                        <img src="asset/images/our__review/star.png" class="img-fluid" alt="">
+                        <img src="asset/images/our__review/star.png" class="img-fluid" alt="">
+                        <img src="asset/images/our__review/star.png" class="img-fluid" alt="">
+                    </div>
+                    <p>“Our members are so impressed. It's intuitive. It's clean. It's distraction free. If you're building a community.</p>
+                    <div class="row our__review__people">
+                        <div class="d-flex">
+                            <div class="our__review__image">
+                                <img src="asset/images/our__review/people1.png" class="img-fluid" alt="">
+                            </div>
+                            <div class="our__review__text">
+                                <h4>Sabo Masties</h4>
+                                <span>Founder @ Rolex</span>
+                            </div>
+                        </div>
+                      
+                    </div>
+                </div>
+            </div>
+            <div class="item">
+                <div class="our__review__items our__review__bg">
+                    <div class="d-flex our__review__star">
+                        <img src="asset/images/our__review/star.png" class="img-fluid" alt="">
+                        <img src="asset/images/our__review/star.png" class="img-fluid" alt="">
+                        <img src="asset/images/our__review/star.png" class="img-fluid" alt="">
+                        <img src="asset/images/our__review/star.png" class="img-fluid" alt="">
+                        <img src="asset/images/our__review/star.png" class="img-fluid" alt="">
+                    </div>
+                    <p>“Our members are so impressed. It's intuitive. It's clean. It's distraction free. If you're building a community.</p>
+                    <div class="row our__review__people">
+                        <div class="d-flex">
+                            <div class="our__review__image">
+                                <img src="asset/images/our__review/people1.png" class="img-fluid" alt="">
+                            </div>
+                            <div class="our__review__text">
+                                <h4>Sabo Masties</h4>
+                                <span>Founder @ Rolex</span>
+                            </div>
+                        </div>
+                      
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+<!-- our Review section Code ends here-->
+
+
+<!-- our forms section starts here -->
+
+<?php include('components/form.php') ?>
+<!-- our forms section ends here -->
+<!-- contact us section starts here -->
+<?php include('components/contact-us.php') ?>
+<!-- contact us section ends here -->
+
+
+
+
+<?php include('include/footer.php') ?>
