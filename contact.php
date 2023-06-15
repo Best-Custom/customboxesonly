c254208d0bfd6e6909ea7042742b3ec84b1ff5a5
Momi786 <ramaljan786@gmail.com>
2023-06-15 22:32:46 +0500

    new page updated

---
+++ contact.php
@@ -0,0 +1,247 @@
+<?php include('include/header.php') ?>
+<!-- contact page section code starts here -->
+
+<section class="contact__page__section">
+    <div class="container">
+        <div class="row">
+            <div class="col-xs-12 col-sm-12 col-md-8 col-lg-8">
+                <div class="contact__form__area p-5">
+                    <h1>Love to hear from you, Get in touch 👋</h1>
+                    <p>Talk to our packaging consultants </p>
+                    <form>
+                    <div class="form-group row">
+                        <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12 mt-3">
+                            <label for="name">What is your name?</label>
+                            <input type="text" name="name" class="form-control mt-1" placeholder="Type name here"> 
+                        </div>
+                        <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12 mt-3">
+                            <label for="name">What is your phone number?</label>
+                            <input type="text" name="email" class="form-control mt-1" placeholder="Type here"> 
+                        </div>
+                    </div>
+                    <div class="form-group row">
+                        <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12 mt-3">
+                            <label for="name">What Your email?</label>
+                            <input type="text" name="name" class="form-control mt-1" placeholder="Type name here"> 
+                        </div>
+                    </div>
+                    <div class="form-group row">
+                        <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12 mt-3">
+                            <label for="name">Please, type your message here</label>
+                            <textarea type="text" name="description" class="form-control mt-1" placeholder="your message here"> </textarea>
+                        </div>
+                    </div> 
+                    <div class="row btn__layout1__positioning justify-content-end mt-3 mb-3">
+                        <button class="btn__layout1 p-3 mt-3">
+                            <a class="d-table" href="#">
+                                <span class="d-table-cell w-100 text-start">send</span>
+                                <img class="d-table-cell" src="asset/images/contact__page/contact__page__vector2.png " alt=""> 
+                            </a>   
+                        </button> 
+                    </div>
+                </form>
+                </div>
+            </div>
+            <div class="col-xs-12 col-sm-12 col-md-4 col-lg-4 mt-3">
+                <div class="contact__page__contact__area p-5">
+                    <div class="">
+                        <div class="contact__us__email mt-3">
+                                <img src="asset/images/contact__page/contact__page__mail__vector.png" class="img-fluid" alt="">
+                        </div>
+                            <h5 class="mt-3">Email Address</h5>
+                            <p class="mt-3 mb-0">info@example.com</p>
+                            <p class="mt-0">support@example.com</p>
+                    </div>
+                    <div class="">
+                        <div class="contact__us__email mt-3">
+                                <img src="asset/images/contact__page/contact__page__call.png" class="img-fluid" alt="">
+                        </div>
+                            <h5 class="mt-3">Phone Number</h5>
+                            <p class="mt-3 mb-0">+88 (800) 123 4567</p>
+                            <p class="mt-0">+99 094 5445 433</p>
+                    </div>
+                    <div class="">
+                        <div class="contact__us__email mt-3">
+                                <img src="asset/images/contact__page/contact__page__location.png" class="img-fluid" alt="">
+                        </div>
+                            <h5 class="mt-3">Our Address</h5>
+                            <p class="mt-3 mb-0">82 12th Street, Office 14, Edinburgh, UK</p>
+                            <p class="mt-0">support@example.com</p>
+                    </div>
+                    <div class="">
+                       <a href="#fb"><img class="contact__page__social__links" src="asset/images/contact__page/contact__page__fb.png" alt=""></a> 
+                       <a href="#twitter"><img class="contact__page__social__links" src="asset/images/contact__page/contact__page__twitter.png" alt=""></a> 
+                       <a href="#instagram"><img class="contact__page__social__links" src="asset/images/contact__page/contact__page__instagram.png" alt=""></a> 
+                       <a href="#linkedin"><img class="contact__page__social__links" src="asset/images/contact__page/contact__page__linkedin.png" alt=""></a> 
+                    </div>
+                </div>
+                
+        </div>
+    </div>
+</section>
+
+<!-- contact page section code starts here -->
+
+<!-- our__journey section Code starts here-->
+
+<section class="our__journey__section">
+    <div class="container">
+        <div class="row">
+            <h1>Simplified Custom Packaging Journey</h1>
+            <p>Unlock the potential of your products with our unrivaled packaging solutions tailored to your industry and specific product needs. </p>
+            <div class="our__journey__item__area row">
+                <div class="col-xs-12 col-sm-12 col-md-3 col-lg-3">
+                    <div class="our__journey__item">
+                        <img src="asset/images/our__journey/our__journey__vector.png" alt="">
+                    </div>
+                    <p class="text-center">Discuss Your Requirements</p>
+                </div>
+                <div class="col-xs-12 col-sm-12 col-md-3 col-lg-3">
+                    <div class="our__journey__item">
+                        <img src="asset/images/our__journey/our__journey__vector.png" alt="">
+                    </div>
+                    <p class="text-center">Discuss Your Requirements</p>
+                </div>
+                <div class="col-xs-12 col-sm-12 col-md-3 col-lg-3">
+                    <div class="our__journey__item">
+                        <img src="asset/images/our__journey/our__journey__vector.png" alt="">
+                    </div>
+                    <p class="text-center">Discuss Your Requirements</p>
+                </div>
+                <div class="col-xs-12 col-sm-12 col-md-3 col-lg-3">
+                    <div class="our__journey__item">
+                        <img src="asset/images/our__journey/our__journey__vector.png" alt="">
+                    </div>
+                    <p class="text-center">Discuss Your Requirements</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+<!-- contact__us section Code starts here-->
+
+
+<!-- our Review section Code starts here-->
+
+<section class="our__review">
+    <div class="container">
+        <div class="row">
+            <h2>See Our Reviews</h2>
+            <div class="row">
+                <div class="col-6">
+                    <p>Unlock the potential of your products with our unrivaled packaging solutions tailored to your industry and specifi.</p>
+                </div>
+                <div class="col-6 text-end">
+                    <a href="#">
+                        <button class="btn__layout1"><span>View All Reviews</span></button>
+                    </a>
+                </div>
+            </div>
+        </div>
+        <div class="owl-carousel owl-theme" id="review__slider">
+            <div class="item">
+                <div class="our__review__items">
+                    <div class="d-flex our__review__star">
+                        <img src="asset/images/our__review/star.png" class="img-fluid" alt="">
+                        <img src="asset/images/our__review/star.png" class="img-fluid" alt="">
+                        <img src="asset/images/our__review/star.png" class="img-fluid" alt="">
+                        <img src="asset/images/our__review/star.png" class="img-fluid" alt="">
+                        <img src="asset/images/our__review/star.png" class="img-fluid" alt="">
+                    </div>
+                    <p>“Our members are so impressed. It's intuitive. It's clean. It's distraction free. If you're building a community.</p>
+                    <div class="row our__review__people">
+                        <div class="d-flex">
+                            <div class="our__review__image">
+                                <img src="asset/images/our__review/people1.png" class="img-fluid" alt="">
+                            </div>
+                            <div class="our__review__text">
+                                <h4>Sabo Masties</h4>
+                                <span>Founder @ Rolex</span>
+                            </div>
+                        </div>
+                      
+                    </div>
+                </div>
+            </div>
+            <div class="item">
+                <div class="our__review__items our__review__bg">
+                    <div class="d-flex our__review__star">
+                        <img src="asset/images/our__review/star.png" class="img-fluid" alt="">
+                        <img src="asset/images/our__review/star.png" class="img-fluid" alt="">
+                        <img src="asset/images/our__review/star.png" class="img-fluid" alt="">
+                        <img src="asset/images/our__review/star.png" class="img-fluid" alt="">
+                        <img src="asset/images/our__review/star.png" class="img-fluid" alt="">
+                    </div>
+                    <p>“Our members are so impressed. It's intuitive. It's clean. It's distraction free. If you're building a community.</p>
+                    <div class="row our__review__people">
+                        <div class="d-flex">
+                            <div class="our__review__image">
+                                <img src="asset/images/our__review/people1.png" class="img-fluid" alt="">
+                            </div>
+                            <div class="our__review__text">
+                                <h4>Sabo Masties</h4>
+                                <span>Founder @ Rolex</span>
+                            </div>
+                        </div>
+                      
+                    </div>
+                </div>
+            </div>
+            <div class="item">
+                <div class="our__review__items">
+                    <div class="d-flex our__review__star">
+                        <img src="asset/images/our__review/star.png" class="img-fluid" alt="">
+                        <img src="asset/images/our__review/star.png" class="img-fluid" alt="">
+                        <img src="asset/images/our__review/star.png" class="img-fluid" alt="">
+                        <img src="asset/images/our__review/star.png" class="img-fluid" alt="">
+                        <img src="asset/images/our__review/star.png" class="img-fluid" alt="">
+                    </div>
+                    <p>“Our members are so impressed. It's intuitive. It's clean. It's distraction free. If you're building a community.</p>
+                    <div class="row our__review__people">
+                        <div class="d-flex">
+                            <div class="our__review__image">
+                                <img src="asset/images/our__review/people1.png" class="img-fluid" alt="">
+                            </div>
+                            <div class="our__review__text">
+                                <h4>Sabo Masties</h4>
+                                <span>Founder @ Rolex</span>
+                            </div>
+                        </div>
+                      
+                    </div>
+                </div>
+            </div>
+            <div class="item">
+                <div class="our__review__items our__review__bg">
+                    <div class="d-flex our__review__star">
+                        <img src="asset/images/our__review/star.png" class="img-fluid" alt="">
+                        <img src="asset/images/our__review/star.png" class="img-fluid" alt="">
+                        <img src="asset/images/our__review/star.png" class="img-fluid" alt="">
+                        <img src="asset/images/our__review/star.png" class="img-fluid" alt="">
+                        <img src="asset/images/our__review/star.png" class="img-fluid" alt="">
+                    </div>
+                    <p>“Our members are so impressed. It's intuitive. It's clean. It's distraction free. If you're building a community.</p>
+                    <div class="row our__review__people">
+                        <div class="d-flex">
+                            <div class="our__review__image">
+                                <img src="asset/images/our__review/people1.png" class="img-fluid" alt="">
+                            </div>
+                            <div class="our__review__text">
+                                <h4>Sabo Masties</h4>
+                                <span>Founder @ Rolex</span>
+                            </div>
+                        </div>
+                      
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+<!-- our Review section Code ends here-->
+
+
+
+<?php include('include/footer.php') ?>
